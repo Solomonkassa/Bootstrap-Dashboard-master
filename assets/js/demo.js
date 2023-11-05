196de164d017f3e36eab66f0733707048169c2a0
solomon kassa <118729276+Solomonkassa@users.noreply.github.com>
2023-11-05 13:07:11 +0300

    Update demo.js

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -16,8 +16,8 @@ Circles.create({
 
 $.notify({
 	icon: 'la la-bell',
-	title: 'Bootstrap notify',
-	message: 'Turning standard Bootstrap alerts into "notify" like notifications',
+	title: 'Solomon Kassa notify',
+	message: 'Turning standard  alerts into "notify" like notifications',
 },{
 	type: 'success',
 	placement: {
